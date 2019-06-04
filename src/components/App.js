ee913b29dc283c3f00b6ace826a141dbd8127919
Mir HB Rahman <ahadcr0@gmail.com>
2019-06-04 22:15:30 +0600

    Added react context system

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
 
-export default class App extends Component {
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
   render() {
     return (
-      <div>
-        App
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     )
   }
 }
+
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import LanguageContext from '../contexts/LanguageContext'
+
+export default class Button extends Component {
+
+  renderSubmit = (value) => {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="ui button primary">
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
+      </div>
+    )
+  }
+}
